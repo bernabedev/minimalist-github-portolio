8dbad5274b2710203592a2afc8ba1812778e90cc
Freilyn Bernabe <freilyn@bernabe.dev>
2025-02-11 18:22:33 -0400

    feat: Added email field and EmailIcon component; new SVG asset

--- src/data.ts
+++ src/data.ts
@@ -14,6 +14,7 @@ export const data = {
         link: 'https://cal.com/freilyn-bernabe-aooipq/15min',
         label: 'Contact Me'
       },
+      email: 'freilyn@bernabe.dev',
       socials: [
         {
           name: 'Github',
@@ -131,6 +132,7 @@ export const data = {
         link: 'https://cal.com/freilyn-bernabe-aooipq/15min',
         label: 'Contáctame'
       },
+      email: 'freilyn@bernabe.dev',
       socials: [
         {
           name: 'Github',
