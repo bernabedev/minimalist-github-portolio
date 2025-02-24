9a6097d4e8434ca165c2fa7ddfdf4a41b712876a
Freilyn Bernabe <freilyn@bernabe.dev>
2025-02-24 11:16:57 -0400

    feat: Configure Astro to use server-side rendering and node adapter
    This commit configures Astro to use server-side rendering and the node adapter for deployment.

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,14 +3,21 @@ import sitemap from '@astrojs/sitemap';
 import tailwind from '@astrojs/tailwind';
 import { defineConfig } from 'astro/config';
 import { loadEnv } from "vite";
+import node from '@astrojs/node';
 const { HOST } = loadEnv(process.env.NODE_ENV ?? '', process.cwd(), "");
 
 // https://astro.build/config
 export default defineConfig({
+  output: 'server',
   site: `https://${HOST ?? 'bernabe.dev'}`,
   integrations: [tailwind(), sitemap()],
+
   i18n: {
     locales: ["es", "en"],
     defaultLocale: "en",
-  }
+  },
+
+  adapter: node({
+    mode: 'standalone'
+  })
 })
